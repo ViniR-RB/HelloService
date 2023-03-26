ff1a8c29e8625e163b568b9cc963c3b50f5b7c65
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2023-03-26 16:04:38 -0300

    utils: Add configurate e namedroutes

---
+++ lib/app/core/utils/namedRoutes.dart
@@ -0,0 +1,7 @@
+abstract class NamedRoutes {
+  NamedRoutes._();
+  static const initial = '/';
+  static const auth = '/auth';
+  static const homePrestador = '/home/prestador/';
+  static const homeEmpresa = '/home/empresa/';
+}
